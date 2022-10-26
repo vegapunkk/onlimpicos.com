c282c3019df77fd4a46996a97a99122a4c0077d6
Onlimpicos <bwuna.gabriele@gmail.com>
2022-10-25 21:57:07 -0300

    teste

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,42 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+
+    <b-navbar toggleable="lg" type="dark" variant="dark" v-if="notIsLoginPage">
+    
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#">Modalidades</b-nav-item>
+          <b-nav-item href="#" >Notícias</b-nav-item>
+          <b-nav-item href="#" >Minha conta</b-nav-item>
+        </b-navbar-nav>
+    
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+    
+          <b-nav-item right>
+            Log Out
+          </b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+
     <router-view/>
   </div>
 </template>
 
+<script>
+  export default{
+    computed:{
+      notIsLoginPage(){
+        return this.$route.name !== "login";
+      }
+    }
+  }
+
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/views/Login.vue
@@ -0,0 +1,69 @@
+<template>
+    <b-row class="vh-100 vw-100 row-login">
+        <b-col sm="5" class="d-flex justify-content-center align-items-center left-login">
+            <div class="col-8">
+                <b-form>
+                    <b-form-group
+                      label=""
+                      label-for=""
+                      >
+                        <b-form-input
+                          id="email"
+                          type="email"
+                          placeholder="Email ou Telefone"
+                          autocomplete="off"
+                          v-model="form.email"
+                        ></b-form-input>
+                    </b-form-group>
+
+                    <b-form-group
+                      label=""
+                      label-for="password"
+                      >
+                        <b-form-input
+                          id="password"
+                          type="password"
+                          placeholder="Senha"
+                          v-model="form.password"
+                        ></b-form-input>
+
+                    </b-form-group>
+                </b-form>
+            </div>
+        </b-col>
+        <b-col sm="7"></b-col>
+    </b-row>
+</template>
+
+<script>
+export default{
+    data() {
+        return{
+            form: {
+                email: "",
+                password:""
+            }
+        }
+    }
+}
+
+</script>
+
+<style>
+*,
+*::after,
+*::before {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    text-decoration: none;
+}
+
+.row-login {
+    margin-left: 0;
+}
+
+.left-login{
+    background-color: #E0D6D0;
+}
+</style>
